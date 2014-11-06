4e5693609e5f884439099a63fd96b64110c8fff9
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2014-11-06 15:42:46 +0100

    strip AmbientLight

--- js/jupiter.js
+++ js/jupiter.js
@@ -24,7 +24,7 @@
         var renderer = new THREE.WebGLRenderer();
         renderer.setSize(width, height);
 
-        scene.add(new THREE.AmbientLight(0x333333));
+        //scene.add(new THREE.AmbientLight(0x333333));
 
         var light = new THREE.DirectionalLight(0xffffff, 1);
         light.position.set(5,3,5);
