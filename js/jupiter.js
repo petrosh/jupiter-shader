1f4c9157dc3cbc643c90b761b014811b0d997854
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2014-11-06 18:29:29 +0100

    alternate stars

--- js/jupiter.js
+++ js/jupiter.js
@@ -77,7 +77,7 @@
                 );
         }
 
-        function createStars(radius, segments) {
+        function createStars1(radius, segments) {
                 return new THREE.Mesh(
                         new THREE.SphereGeometry(radius, segments, segments),
                         new THREE.MeshBasicMaterial({
@@ -86,5 +86,61 @@
                         })
                 );
         }
+        function createStars(radius, segments) {
+          // stars
 
+				var i, r = radius, starsGeometry = [ new THREE.Geometry(), new THREE.Geometry() ];
+
+				for ( i = 0; i < 250; i ++ ) {
+
+					var vertex = new THREE.Vector3();
+					vertex.x = Math.random() * 2 - 1;
+					vertex.y = Math.random() * 2 - 1;
+					vertex.z = Math.random() * 2 - 1;
+					vertex.multiplyScalar( r );
+
+					starsGeometry[ 0 ].vertices.push( vertex );
+
+				}
+
+				for ( i = 0; i < 1500; i ++ ) {
+
+					var vertex = new THREE.Vector3();
+					vertex.x = Math.random() * 2 - 1;
+					vertex.y = Math.random() * 2 - 1;
+					vertex.z = Math.random() * 2 - 1;
+					vertex.multiplyScalar( r );
+
+					starsGeometry[ 1 ].vertices.push( vertex );
+
+				}
+
+				var stars;
+				var starsMaterials = [
+					new THREE.PointCloudMaterial( { color: 0x555555, size: 2, sizeAttenuation: false } ),
+					new THREE.PointCloudMaterial( { color: 0x555555, size: 1, sizeAttenuation: false } ),
+					new THREE.PointCloudMaterial( { color: 0x333333, size: 2, sizeAttenuation: false } ),
+					new THREE.PointCloudMaterial( { color: 0x3a3a3a, size: 1, sizeAttenuation: false } ),
+					new THREE.PointCloudMaterial( { color: 0x1a1a1a, size: 2, sizeAttenuation: false } ),
+					new THREE.PointCloudMaterial( { color: 0x1a1a1a, size: 1, sizeAttenuation: false } )
+				];
+
+				for ( i = 10; i < 30; i ++ ) {
+
+					stars = new THREE.PointCloud( starsGeometry[ i % 2 ], starsMaterials[ i % 6 ] );
+
+					stars.rotation.x = Math.random() * 6;
+					stars.rotation.y = Math.random() * 6;
+					stars.rotation.z = Math.random() * 6;
+
+					s = i * 10;
+					stars.scale.set( s, s, s );
+
+					stars.matrixAutoUpdate = false;
+					stars.updateMatrix();
+
+					scene.add( stars );
+
+				}
+      }
 }());
