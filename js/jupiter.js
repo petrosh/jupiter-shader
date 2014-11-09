42dba84e2b4fdba747895a00aa45c24b5f86e986
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2014-11-09 17:13:12 +0100

    again

--- js/jupiter.js
+++ js/jupiter.js
@@ -99,8 +99,8 @@ function init() {
   neck = new THREE.Object3D();
   // neck.rotateOnAxis(new THREE.Vector3(1, 0, 0), degInRad(90));
   neck.up = new THREE.Vector3(0, 0, 1);
-  neck.position.z = 1;
-  neck.position.y = -5;
+  // neck.position.z = 1;
+  // neck.position.y = -5;
 
   neck.add(camera);
   scene.add(neck);
